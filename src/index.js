b164fd0c37c99a6386f27a397f79b6193022ec80
Jeff Baumgardt <medros1455@gmail.com>
2018-04-24 12:35:50 -0600

    Add flow types, frist pass. Also add a dispatch to initilize the newly added reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
+// @flow
+
 import { combineReducers } from 'redux'
+import type { Reducer, StoreEnhancer } from 'redux'
 
 let asyncReducers = {}
-export function asyncStoreEnhancer(initReducers) {
-
-	const makeRootReducer = (asyncReducers) => {
+export function asyncStoreEnhancer<S, A, D>(initReducers: ?Object) {
+	const makeRootReducer = (asyncReducers: Object) => {
 		return combineReducers({
 			...initReducers,
 			...asyncReducers
 		})
 	}
 
-	return next => (reducers, initialState, enchancer) => {
+	return (next: function) => (reducers: Reducer<S, A>, initialState: S, enchancer: StoreEnhancer<S, A, D>) => {
 		const store = next(reducers, initialState, enchancer)
 
 		function injectReducer(key, reducer) {
 			if (!asyncReducers[key]) {
 				asyncReducers[key] = reducer
 				store.replaceReducer(makeRootReducer(asyncReducers))
+				store.dispatch({ type: '@@redux-async-enhancer/injectReducer' })
 			}
 		}
 
